70cbc13a751608abb69f86cbb48cabd2d5a6b7d5
pcdoccs <rnskm101112@gmail.com>
2024-08-19 01:13:45 +0530

    Create TRANSENCRYPT.c

---
+++ TRANSENCRYPT.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("Enter the data to be encrypted: ");
+    char ch, data[30][30], output[100], seq[] = "MEGABUCK";
+    int i, j, k, Columns = 0, Rows = 0, seq_No = 0, len = strlen(seq);
+    
+    while ((ch = getchar()) != '\n') { // Read input data into the matrix
+        data[Rows][Columns++] = ch;
+        if(Columns == len){
+            Rows++;
+            Columns=0;
+        }
+    }
+    if (Columns != 0) { // Pad the remaining cells with dots
+        for (i = Columns; i < len; i++) {
+            data[Rows][i] = '.';
+        }
+        Rows++;
+    }
+    for (i = 0; i < len; i++) {
+        seq_No = 0;
+        for (j = 0; j < len; j++) { 
+            if (seq[i] > seq[j])
+                seq_No++; // Count gives the sequence order based on the keyword
+        }
+        for (j = 0; j < Rows; j++) { // Create the output based on the sequence order
+            if ((seq_No * Rows) + j >= Rows * len)
+                break;
+            output[(seq_No * Rows) + j] = data[j][i];
+        }
+    }
+    output[Rows * len] = '\n';
+    printf("\nEncrypted data: ");
+    for (i = 0; i < Rows * len; i++) {
+        printf("%c", output[i]);
+    }
+    printf("\n");
+    return 0;
+}
